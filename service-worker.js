9365787bbd5137e318ab8f70489e7cccd54ef6ac
Robin Wyss <rowyss@gmail.com>
2018-09-04 13:35:48 -0700

    updated icons

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,25 @@
 // Cache names
-var dataCacheName = 'QuoteClock-v0.7'
-var cacheName = "QuoteClock-1.1"
+var dataCacheName = 'CQ-v0.2'
+var cacheName = "CQ-1.00"
 
 // Application shell files to be cached
-var filesToCache = ['/', '/index.html', '/app.js',
-	'/styles.css', 'data.json', 'https://fonts.googleapis.com/css?family=Abel',
-	'/images/apple-touch-icon.png', '/images/mstile-150x150.png', '/favicon-16x16.png',
-	'/favicon-32x32.png', '/images/android-chrome-512x512.png', 
-	'/images/android-chrome-192x192.png', '/images/safari-pinned-tab.svg'
+var filesToCache = [
+	'/index.html',
+	'/app.js',
+	'/styles.css',
+	'/data.json' ,
+	'https://fonts.googleapis.com/css?family=Abel',
+	'/apple-touch-icon.png',
+	'/images/icons/icon-72x72.png',
+	'/images/icons/icon-96x96.png',
+	'/images/icons/icon-128x128.png',
+	'/images/icons/icon-144x144.png',
+	'/images/icons/icon-152x152.png',
+	'/images/icons/icon-192x192.png',
+	'/images/icons/icon-384x384.png',
+	'/images/icons/icon-512x512.png',
+	'/favicon-16x16.png',
+	'/favicon-32x32.png'
 ]
 
 self.addEventListener('install', function (e) {
